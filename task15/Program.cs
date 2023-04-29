5d769796bc79acab111d25ebc5f7a32790657f73
Andrey Sidorov <rxsid@mail.ru>
2023-04-29 14:02:42 +0300

    added task15

---
+++ task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n >5 && n <8)
+    {
+    Console.WriteLine("Выходной");
+    }
+else if(n > 0 && n <6)
+    {
+    Console.WriteLine("Рабочий день");
+    }
+else 
+    {
+    Console.WriteLine("Не существующий номер дня недели");
+    }
