35c2485ff53fd93bba8f81a32819fc2d8272d641
Andrey Sidorov <rxsid@mail.ru>
2023-04-29 13:45:33 +0300

    added task10 and task13

---
+++ task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.Write("Введите чило: ");
+string num = Console.ReadLine();
+if(num.Length > 2)
+{
+    char threeChar = num[2];
+    Console.WriteLine(threeChar);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
